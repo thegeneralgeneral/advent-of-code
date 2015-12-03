d080afe418ce33f7f3700b0e8f1c18b216af548e
J Fingler <jenna.fingler@gmail.com>
2015-12-03 01:24:16 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # Day 1
-import day1
-print day1.calculate_floor(day1.INPUT_STRING)
-print day1.calculate_basement_step(day1.INPUT_STRING)
+# import day1
+# print day1.calculate_floor(day1.INPUT_STRING)
+# print day1.calculate_basement_step(day1.INPUT_STRING)
+
+# Day 2
+
+inputs = day2.INPUT_STRING.split()
+print len(inputs)
+lwh_tuples = [tuple(l.split('x')) for l in inputs]
+total = sum([day2.get_paper_area(int(l), int(w), int(h)) for l, w, h in lwh_tuples])
+print total

14cfc82ac82af44a1da4c9b7a2e83b738d3ef72f
thegeneralgeneral <jenna.fingler@gmail.com>
2015-12-06 14:38:10 +0000

    Completed 1st part of Day 6 (before changing implementation for Part 2)

--- main.py
+++ main.py
@@ -27,16 +27,22 @@
 # result = day4.get_suffix_num_resulting_in_six_zeroes("iwrupvqb")
 # print result
 
-import day5
-inputs = day5.INPUT_STRING.split('\n')
-num_nice = 0
-for i in inputs:
-    if day5.is_nice(i):
-        num_nice += 1
-print "Num nice: %s" % num_nice
+# import day5
+# inputs = day5.INPUT_STRING.split('\n')
+# num_nice = 0
+# for i in inputs:
+#     if day5.is_nice(i):
+#         num_nice += 1
+# print "Num nice: %s" % num_nice
 
-num_nice = 0
-for s in inputs:
-    if day5.is_nice_2(s):
-        num_nice += 1
-print "Num nice 2: %s" % num_nice  # 203 is too high
+# num_nice = 0
+# for s in inputs:
+#     if day5.is_nice_2(s):
+#         num_nice += 1
+# print "Num nice 2: %s" % num_nice  # 203 is too high
+
+import day6
+grid = day6.get_1000_by_1000_grid()
+for i in day6.INPUT_STRING.split('\n'):
+    grid = day6.apply_instruction_string_to_grid(i, grid)
+print day6.get_num_lights_on(grid)
